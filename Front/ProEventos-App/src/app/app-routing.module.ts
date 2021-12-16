b59996a39c17379cc6a35461b3c6cc3fa8a08de5
AndreCypriano23 <andrecypriano23@outlook.com>
2021-12-16 13:13:45 -0300

    Angular Routes, Alerts and Organization

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashborad.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'palestrantes', component: PalestrantesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },/*Quando nao for nada, tipo index eu quero isso */
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }/*se nao for qq outra coisa de antes */
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
